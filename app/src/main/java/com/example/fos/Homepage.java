4aaefc889d9f7eeede94302b92dcd34e9395f4c7
abhi4j <abhi559416@yahoo.com>
2015-02-08 12:03:23 +0530

    Setting and java files

---
+++ app/src/main/java/com/example/fos/Homepage.java
@@ -0,0 +1,55 @@
+package com.example.fos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Homepage extends Activity implements View.OnClickListener {
+    Button login1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        login1 = (Button)findViewById(R.id.login1);
+
+        login1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_homepage, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent logop = new Intent(this, Login.class);
+        startActivity(logop);
+        finish();
+    }
+}
